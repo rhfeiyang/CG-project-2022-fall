3c77ab4e8f4c4c6b6b01a42b745b6a3d83d8e28f
LeeeeTX <liya1@shanghaitech.edu.cn>
2022-12-19 19:08:12 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 // Final project of CG in ShanghaiTech
-// Multi-Resolution Isosurface Rendering
+// Multi-Resolution Iso-surface Rendering
 // By Renhui Liyian Songhaiyu
 
 #include <iostream>
 
 int main(){
     std::cout << "Hello world" << std::endl;
-    printf("2121");
-//    for(auto &:){
-//        
-//        fe
-//    }
     return 0;
 }

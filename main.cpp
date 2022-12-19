9422dff3c65bee6c55f9bf3d1286bdbc8e7e1b9b
rhfeiyang <rhfeiyang@qq.com>
2022-12-19 21:34:12 +0800

    cmakelist

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 // Final project of CG in ShanghaiTech
 // Multi-Resolution Iso-surface Rendering
 // By Renhui Liyian Songhaiyu
-
+#include <openvdb/openvdb.h>
 #include <iostream>
 
+using Vec3f=openvdb::Vec3f;
+
 int main(){
-    std::cout << "Hello world" << std::endl;
+    Vec3f  v={1,2,3};
+    std::cout << v[0]<<v[1]<<v[2] << std::endl;
     return 0;
 }

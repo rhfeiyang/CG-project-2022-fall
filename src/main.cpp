c20fc04862168c82318afce17dd01398e04f8f65
So-cean <913902676@qq.com>
2023-01-01 13:04:03 +0800

    ocean

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 #include "VDBLoader.cpp"
 #include "common.h"
 #include <openvdb/openvdb.h>
+#include "Cell.h"
 
 int main() {
+    //VDBLoader<openvdb::FloatGrid > loader("../resource/bunny.vdb");
     VDBLoader<openvdb::Vec3SGrid > loader("../resource/multi-res small.vdb");
 //Construct ABR
 
     for (auto &grid: loader.grids) {
 
-
-
         //get meta data
-        for(auto iter=grid->beginMeta();iter!=grid->endMeta();++iter){
-            const std::string& name = iter->first;
+        for (auto iter = grid->beginMeta(); iter != grid->endMeta(); ++iter) {
+            const std::string &name = iter->first;
             openvdb::Metadata::Ptr value = iter->second;
             std::string valueAsString = value->str();
             std::cout << name << " = " << valueAsString << std::endl;
@@ -23,8 +23,12 @@ int main() {
         //value iteration
         for (auto iter = grid->cbeginValueOn(); iter.test(); ++iter) {
             auto dist = *iter;
-            //cout << dist << endl;
+            auto bbox = iter.getBoundingBox();
+            //cout << dist << bbox << endl;
+            //Cell cell;
+
         }
+
     }
 //Draw
 
